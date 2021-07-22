49e13af0595a151a69811cfe4f4f298a0f63652e
sam-trolland <sam.trolland@monash.edu>
2021-07-22 20:26:48 +1000

    MidiNotePassFilter v1.0.0 + VST3 Plugin to filter out all Midi messages except NoteOn and Note Off events.

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,193 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+MidiNotePassFilterAudioProcessor::MidiNotePassFilterAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+}
+
+MidiNotePassFilterAudioProcessor::~MidiNotePassFilterAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String MidiNotePassFilterAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool MidiNotePassFilterAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool MidiNotePassFilterAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool MidiNotePassFilterAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double MidiNotePassFilterAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int MidiNotePassFilterAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int MidiNotePassFilterAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void MidiNotePassFilterAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String MidiNotePassFilterAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void MidiNotePassFilterAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void MidiNotePassFilterAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void MidiNotePassFilterAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool MidiNotePassFilterAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void MidiNotePassFilterAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    MidiBuffer generatedMidi;
+    
+    for (const auto meta : midiMessages)
+    {
+        auto msg = meta.getMessage();
+        msg.setTimeStamp(Time::getApproximateMillisecondCounter() * 0.001);
+        if(msg.isNoteOnOrOff()){
+            //msg.setChannel(2);
+            generatedMidi.addEvent(msg, 0);
+        }
+    }
+    
+    // Remove Raw midi input and only transmit dinverno generated messages
+    midiMessages.swapWith(generatedMidi);
+}
+
+//==============================================================================
+bool MidiNotePassFilterAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* MidiNotePassFilterAudioProcessor::createEditor()
+{
+    return new MidiNotePassFilterAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void MidiNotePassFilterAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void MidiNotePassFilterAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new MidiNotePassFilterAudioProcessor();
+}
