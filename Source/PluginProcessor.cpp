fce6466dc68a0d68a4d3dd75bb884552a4c39411
sam-trolland <sam.trolland@monash.edu>
2021-07-22 22:00:28 +1000

    Removed VST Audio Input/Outputs

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -15,9 +15,9 @@ MidiNotePassFilterAudioProcessor::MidiNotePassFilterAudioProcessor()
      : AudioProcessor (BusesProperties()
                      #if ! JucePlugin_IsMidiEffect
                       #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                       //.withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
                       #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                       //.withOutput ("Output", juce::AudioChannelSet::stereo(), true)
                      #endif
                        )
 #endif
